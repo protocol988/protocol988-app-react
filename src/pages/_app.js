46993d9760317892520672ed7ca7a976f182cb13
Tomas Taylor <totaylor19@gmail.com>
2023-09-09 16:20:05 -0500

    Wallet integration and dashboard components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,82 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+  goerli,
+} from "wagmi/chains";
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { publicProvider } from "wagmi/providers/public";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const projectId = "YOUR_PROJECT_ID";
+
+const { wallets } = getDefaultWallets({
+  appName: "Protocol 988",
+  projectId,
+  chains,
+});
+
+const demoAppInfo = {
+  appName: "Protocol 988",
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        theme={darkTheme()}
+        appInfo={demoAppInfo}
+        chains={chains}
+      >
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <meta property="og:title" content="Protocol 988" />
+        <meta
+          property="og:description"
+          content="The leading forward contracts DeFi platform"
+        />
+        <meta
+          name="description"
+          content="The leading forward contracts DeFi platform"
+          key="desc"
+        />
+        <meta property="og:image" content="/img/logo.jpg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
